f0cf31ddea5078e68f2fc7e0e17d9328ccb43c3a
silly <silly@github.com>
2023-07-30 23:39:24 +0300

    fuck migrations

--- prisma/migrations/20230729215926_notepad_author/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `authorUsername` to the `Notepad` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Notepad" ADD COLUMN     "authorUsername" TEXT NOT NULL,
-ADD COLUMN     "usernameAuthTag" TEXT;
